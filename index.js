964ac5753239e4de81a793bb5798019e9405f423
oskah <oskah@uef.fi>
2025-06-27 19:39:58 +0300

    13.4 turned app into web app with some basic route handling

--- index.js
+++ index.js
@@ -1,16 +1,96 @@
 require('dotenv').config()
-const { Sequelize } = require('sequelize')
+const express = require('express')
+const { Sequelize, Model, DataTypes } = require('sequelize')
+
+const app = express()
+app.use(express.json())
 
 const sequelize = new Sequelize(process.env.DATABASE_URL)
 
-const main = async () => {
-  try {
-    await sequelize.authenticate()
-    console.log('Connection has been established successfully.')
-    sequelize.close()
-  } catch (error) {
-    console.error('Unable to connect to the database:', error)
+class Blog extends Model {}
+Blog.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  author: {
+    type: DataTypes.STRING
+  },
+  url: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  likes: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
   }
-}
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'blog'
+})
 
-main()
+sequelize.sync()
+
+app.get('/api/blogs', async (req, res) => {
+  const blogs = await Blog.findAll()
+  res.json(blogs)
+})
+
+app.get('/api/blogs/:id', async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id)
+  if (blog) {
+    res.json(blog)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.post('/api/blogs', async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body)
+    res.status(201).json(blog)
+  } catch (error) {
+    console.error(error)
+
+    if (error.name === 'SequelizeValidationError') {
+      return res.status(400).json({ error: error.errors.map(e => e.message) })
+    }
+
+    res.status(500).json({ error: 'Something went wrong' })
+  }
+})
+
+
+app.put('/api/blogs/:id', async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id)
+  if (blog) {
+    blog.likes = req.body.likes
+    await blog.save()
+    res.json(blog)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.delete('/api/blogs/:id', async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id)
+
+  if (blog) {
+    await blog.destroy()
+    res.status(204).end()
+  } else {
+    res.status(404).json({ error: 'Blog not found' })
+  }
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
