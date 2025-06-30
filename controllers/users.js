e3d6be3f1d6a397d388d6f2eb64dc218bf189f22
oskah <oskah@uef.fi>
2025-06-30 12:44:36 +0300

    13.8 initial setup for users and login

---
+++ controllers/users.js
@@ -0,0 +1,26 @@
+const router = require('express').Router()
+const { User } = require('../models')
+
+router.get('/', async (req, res) => {
+  const users = await User.findAll()
+  res.json(users)
+})
+
+router.post('/', async (req, res) => {
+  const user = await User.create(req.body)
+  res.json(user)
+})
+
+router.put('/:username', async (req, res) => {
+  const user = await User.findOne({ where: { username: req.params.username } })
+
+  if (user) {
+    user.username = req.body.username || user.username
+    await user.save()
+    res.json(user)
+  } else {
+    res.status(404).json({ error: 'User not found' })
+  }
+})
+
+module.exports = router
