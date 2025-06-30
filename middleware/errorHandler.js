a5ba7a31936b6e8d9ae24b57b58715df018b8ad2
oskah <oskah@uef.fi>
2025-06-30 20:24:00 +0300

    13.9 email username validation with sequelize

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -2,7 +2,7 @@ const { ValidationError } = require('sequelize')
 
 const errorHandler = (error, req, res, next) => {
   if (error instanceof ValidationError) {
-    return res.status(400).json({ error: error.message })
+    return res.status(400).json({ error: error.errors.map(e => e.message) })
   }
 
   if (error.name === 'SequelizeDatabaseError') {
