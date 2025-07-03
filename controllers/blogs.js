8af0a1cd49acf5898a66feaca472800b3c793428
oskah <oskah@uef.fi>
2025-07-03 14:15:14 +0300

    13.11 deleting your own blogs

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -25,8 +25,19 @@ router.put('/:id', blogFinder, async (req, res) => {
   res.json(req.blog)
 })
 
-router.delete('/:id', blogFinder, async (req, res) => {
-  await req.blog.destroy()
+router.delete('/:id', tokenExtractor, blogFinder, async (req, res) => {
+  const blog = req.blog
+  const tokenUserId = req.decodedToken.id
+
+  if (!blog) {
+    return res.status(404).json({ error: 'blog not found' })
+  }
+
+ if (!blog.userId || blog.userId.toString() !== tokenUserId.toString()) {
+  return res.status(403).json({ error: 'unauthorized: you can only delete your own blogs' })
+}
+
+  await blog.destroy()
   res.status(204).end()
 })
 
