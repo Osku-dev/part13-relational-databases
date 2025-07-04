36f6025dd0d1f3bc8aa31199d214eeabe997228e
oskah <oskah@uef.fi>
2025-07-04 12:46:16 +0300

    13.14 expanding filtering to include author field

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -9,16 +9,27 @@ router.get('/', async (req, res) => {
   const where = {}
 
   if (req.query.search) {
-    where.title = {
-      [Op.iLike]: `%${req.query.search}%`
-    }
+    where[Op.or] = [
+      {
+        title: {
+          [Op.iLike]: `%${req.query.search}%`
+        }
+      },
+      {
+        author: {
+          [Op.iLike]: `%${req.query.search}%`
+        }
+      }
+    ]
   }
+
   const blogs = await Blog.findAll({
     where,
     include: {
       model: User
     }
   })
+
   res.json(blogs)
 })
 
